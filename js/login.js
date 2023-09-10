ac6d83b40620eb062ecbe572a1785d69dd3d03f7
Agustin Togni <Agustintogni2003@yahoo.com>
2023-09-10 16:35:06 -0300

    Se agregan validaciones a las logicas de register y login

--- js/login.js
+++ js/login.js
@@ -1,12 +1,17 @@
 // Se declaran los componentes del login.
-const formLogin = document.querySelector('#formLogin')
-const inputTuMail = document.querySelector('#inputTuMail')
-const inputTuPassword = document.querySelector('#inputTuPassword')
+const formLogin = document.querySelector('#formLogin');
+const inputTuMail = document.querySelector('#inputTuMail');
+const inputTuPassword = document.querySelector('#inputTuPassword');
 
-const navRegister = document.querySelector('#navRegister')
-const textoRegistrate = document.querySelector('#textoRegistrate')
+const navRegister = document.querySelector('#navRegister');
+const textoRegistrate = document.querySelector('#textoRegistrate');
 
-const navLogin = document.querySelector('#navLogin')
+const navLogin = document.querySelector('#navLogin');
+
+// Se crea un elemento "p" para mostrar el mensaje de error.
+const p = document.createElement('p');
+p.textContent = 'Completa el campo.';
+p.className = 'alertas';
 
 // Verifica si hay datos de usuarios almacenados en el local storage.
 const usuariosGuardados = JSON.parse(localStorage.getItem('claveNuevoUsuario'));
@@ -32,27 +37,41 @@ formLogin.addEventListener('submit', (event) => {
     // Obtiene los usuarios guardados del localStorage.
     const usuariosGuardados = JSON.parse(localStorage.getItem('claveNuevoUsuario'));
 
-    // Verifica si hay datos de usuarios almacenados.
-    if (usuariosGuardados && usuariosGuardados.length > 0) {
-        // Realiza la comparacion del mail y password ingresados con los datos almacenados.
-        const usuarioEncontrado = usuariosGuardados.find((usuario) => {
-            return usuario.mail === mailIngresado && usuario.password === passwordIngresado;
-        });
-
-        if (usuarioEncontrado) {
-            // En caso de coincidir los datos se redirecciona al index html.
-            window.location.href = '../index.html';
-        } else {
-            // En caso de no coincidir los datos aparece una alerta.
-            textoRegistrate.innerHTML = 'Credenciales incorrectas. Verifica tu correo y contraseña.';
-            textoRegistrate.className = 'alertas'
-        }
+    if (mailIngresado.trim() === '') {
+        // Insertamos el mensaje de error antes del inputTuMail.
+        inputTuMail.parentNode.insertBefore(p, inputTuMail.nextSibling);
+    } else if (passwordIngresado.trim() === '') {
+        // Insertamos el mensaje de error antes del inputTuPassword.
+        inputTuPassword.parentNode.insertBefore(p, inputTuPassword.nextSibling);
     } else {
-        // Si no hay datos de usuarios almacenados, muestra un mensaje de error.
-        textoRegistrate.innerHTML = 'Esa cuenta no se encuentra registrada. Primero registrate haciendo click arriba a la derecha.';
-        textoRegistrate.className = 'alertas'
+        // Verifica si hay datos de usuarios almacenados.
+        if (usuariosGuardados && usuariosGuardados.length > 0) {
+            // Realiza la comparación del mail y password ingresados con los datos almacenados.
+            const usuarioEncontrado = usuariosGuardados.find((usuario) => {
+                return usuario.mail === mailIngresado && usuario.password === passwordIngresado;
+            });
+
+            if (usuarioEncontrado) {
+                // En caso de coincidir los datos se redirecciona al index html.
+                window.location.href = '../index.html';
+            } else {
+                // En caso de no coincidir los datos aparece una alerta.
+                textoRegistrate.innerHTML = 'Credenciales incorrectas. Verifica tu correo y contraseña.';
+                textoRegistrate.className = 'alertas';
+            }
+        } else {
+            // Se borra cualquier mensaje de error presente.
+            if (p !== '') {
+                p.remove();
+            }
+
+            // Si no hay datos de usuarios almacenados, muestra un mensaje de error.
+            textoRegistrate.innerHTML = 'Esa cuenta no existe aún, hace <a href="./register.html">click acá</a> y registrate!';
+            textoRegistrate.className = 'alertas';
+        }
     }
 });
+
 
 
 
--- js/register.js
+++ js/register.js
@@ -1,22 +1,28 @@
 // Se crea la clase para el constructor de usuarios.
 class Usuarios {
     constructor({ nombre, apellido, mail, password }) {
-        this.nombre = nombre
-        this.apellido = apellido
-        this.mail = mail
-        this.password = password
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.password = password;
     }
 }
 
 // Se declaran los componentes del register.
-const formRegister = document.querySelector('#formRegister')
-const inputNombre = document.querySelector('#inputNombre')
-const inputApellido = document.querySelector('#inputApellido')
-const inputMail = document.querySelector('#inputMail')
-const inputPassword = document.querySelector('#inputPassword')
+const formRegister = document.querySelector('#formRegister');
+const inputNombre = document.querySelector('#inputNombre');
+const inputApellido = document.querySelector('#inputApellido');
+const inputMail = document.querySelector('#inputMail');
+const inputPassword = document.querySelector('#inputPassword');
+const inputNombreApellido = document.querySelector('#inputNombreApellido');
+
+// Se crea un elemento "p" para mostrar el mensaje de error.
+const p = document.createElement('p');
+p.textContent = 'Completa el campo.';
+p.className = 'alertas';
 
 // Se declara un array para almacenar los usuarios.
-const usuarios = []
+const usuarios = [];
 
 // Se agrega la escucha del evento al formulario.
 formRegister.addEventListener('submit', (event) => {
@@ -24,19 +30,45 @@ formRegister.addEventListener('submit', (event) => {
     event.preventDefault();
 
     // Se guardan los valores cargados en los campos.
-    const nombre = inputNombre.value
-    const apellido = inputApellido.value
-    const mail = inputMail.value
-    const password = inputPassword.value
+    const nombre = inputNombre.value;
+    const apellido = inputApellido.value;
+    const mail = inputMail.value;
+    const password = inputPassword.value;
 
-    // Se crea un usuario a partir de los datos guardados.
-    const nuevoUsuario = new Usuarios({ nombre, apellido, mail, password })
+    // Verificamos si los campos nombre y apellido están vacíos.
+    if (nombre.trim() === '' || apellido.trim() === '') {
 
-    // Se guarda el usuario creado en el local storage.
-    usuarios.push(nuevoUsuario)
-    localStorage.setItem('claveNuevoUsuario', JSON.stringify(usuarios))
+        // Insertamos el mensaje de error antes del inputNombreApellido.
+        inputNombreApellido.parentNode.insertBefore(p, inputNombreApellido.nextSibling);
 
-    // Redirige al html de login.
-    window.location.href = 'login.html';
-})
+    } else if (mail.trim() === '') {
+
+        // Insertamos el mensaje de error antes del inputMail.
+        inputMail.parentNode.insertBefore(p, inputMail.nextSibling);
+
+    } else if (password.trim() === '') {
+
+        // Insertamos el mensaje de error antes del inputPassword.
+        inputPassword.parentNode.insertBefore(p, inputPassword.nextSibling);
+
+    } else {
+
+        // Eliminamos cualquier mensaje de error existente.
+        const mensajeError = inputNombreApellido.nextSibling;
+        if (mensajeError) {
+            mensajeError.remove();
+        }
+
+        // Se crea un usuario a partir de los datos guardados.
+        const nuevoUsuario = new Usuarios({ nombre, apellido, mail, password });
+
+        // Se guarda el usuario creado en el local storage.
+        usuarios.push(nuevoUsuario);
+        localStorage.setItem('claveNuevoUsuario', JSON.stringify(usuarios));
+
+        // Redirige al HTML de login.
+        window.location.href = 'login.html';
+    }
+});
+
 
